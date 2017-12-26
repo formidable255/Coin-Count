141ce0d098c7e8824574287f283cabe2a2b6b134
David Lu <d4v1d_lu@hotmail.com>
2017-12-26 15:22:45 -0800

    offset bootstrap

--- index.php
+++ index.php
@@ -33,9 +33,8 @@
         </div>
     </div>
     <div class="row">
-    	<div class="col-lg-5 col-md-5 col-sm-5 col-xs-5">
-    	</div>
-        <div class="col-lg-7 col-md-7 col-sm-7 col-xs-7 ">
+    	
+        <div class="offset-lg-5 offset-md-5 offset-sm-5 offset-xs-5 col-lg-7 col-md-7 col-sm-7 col-xs-7 ">
           <input type="submit" name="submit" value="Calculate Coins" class="myButton"/>
           <input type="reset" name="reset" value="Clear form" class="myButton" />
         </div>
